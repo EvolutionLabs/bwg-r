a98c3732a4f6a676f1fcf4de485638c66f02e5fb
Andrei Gheorghiu <andrei.gheorghiu@evolution.ro>
2017-01-11 20:02:09 +0200

    added top image. no parallax yet

--- parts/pages/section.php
+++ parts/pages/section.php
@@ -25,6 +25,13 @@ $dd = [
 ];
 $category = isset($_GET['category']) ? $_GET['category'] : 'alcohol'
 ?>
+<div style="background: #eee url('../../assets/image/banner.jpg') no-repeat 50% 50% /cover;" class="parallax-scroller">
+	<div class="scene container">
+		<h3>New Season Offers</h3>
+		<div class="subtitle">2 Cases for the price of 1</div>
+		<a href="#" class="btn btn-success">Buy Now</a>
+	</div>
+</div>
 <div class="submenu <?= $_GET['category']; ?>">
 	<div class="container">
 		<nav class="navbar">
