4de8e1d97b7a67d00c8c8c67c8d2a0fc306e8c1e
Andrei Gheorghiu <andrei.gheorghiu@evolution.ro>
2017-01-13 18:16:17 +0200

    closing child collapse on parent closing

--- assets/js/shoplink.js
+++ assets/js/shoplink.js
@@ -1,5 +1,12 @@
 $(window).on('load', function(){
     $('[href^="#"]').on('click',function(e){
         e.preventDefault();
+    });
+    $('table').on('hide.bs.collapse', 'tr', function(e){
+        if (e.target.id.indexOf('tr-') > -1) {
+            var id = e.target.id.substr(e.target.id.indexOf('-') + 1);
+            $('#stock-' + id).collapse('hide');
+        }
     })
+
 });
