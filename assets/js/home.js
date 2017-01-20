a0381b14aac764bab3372dc94f7968ed8b708eef
Andrei Gheorghiu <andrei.gheorghiu@evolution.ro>
2017-01-20 19:41:17 +0200

    added landing opener animation for narrow devices

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,13 @@
 $(window).on('load', function(){
     $('#hhtErrorsModal').on('show.bs.modal', function(){
-        if (!window.tabAlreadySet) {
+        if (!window.tabIsSet) {
             var height = $('.tab-pane.active').height();
             if (height) {
                 $('.tab-content').velocity({
                     maxHeight:height + 'px',
                     minHeight:height + 'px'
                 });
-                window.tabAlreadySet = true;
+                window.tabIsSet = true;
             }
         }
     });
@@ -20,22 +20,37 @@ $(window).on('load', function(){
     }, 200, function(){
         loader.css({'background-color':'transparent'});
         loader.find('div').each(function(i){
-            $(this).velocity({
-                'margin-left':(i % 2 == 0 ? '0': '10px'),
-                'margin-right':(i % 2 == 0 ? '10px': '0')
-            }, 120).velocity({
-                'margin-top':(i < 2 ?'0':'10px'),
-                'margin-bottom':(i < 2 ?'10px':'0')
-            }, 120, function(){
-                $('.shop-links>div:last-child>span').each(
-                    function(){
-                        $(this).velocity({opacity: 1},500, function(){
-                            loader.remove();
-                            $('#hhtErrorsModal').modal('show');
-                        })
-                    }
-                )
-            });
+            if ($(window).width() > 820) {
+                $(this).velocity({
+                    'margin-left':(i % 2 == 0 ? '0': '10px'),
+                    'margin-right':(i % 2 == 0 ? '10px': '0')
+                }, 120).velocity({
+                    'margin-top':(i < 2 ?'0':'10px'),
+                    'margin-bottom':(i < 2 ?'10px':'0')
+                }, 120, function(){
+                    $('.shop-links>div:last-child>span').each(
+                        function(){
+                            $(this).velocity({opacity: 1},500, function(){
+                                loader.remove();
+                                $('#hhtErrorsModal').modal('show');
+                            })
+                        }
+                    )
+                });
+            } else {
+                $(this).velocity({
+                    'margin-top':'10px'
+                },120, function(){
+                    $('.shop-links>div:last-child>span').each(
+                        function(){
+                            $(this).velocity({opacity: 1},500, function(){
+                                loader.remove();
+                                $('#hhtErrorsModal').modal('show');
+                            })
+                        }
+                    );
+                });
+            }
         })
     });
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
